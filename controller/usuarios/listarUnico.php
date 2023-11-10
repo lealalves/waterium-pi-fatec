6a6e6e23c26218bc2bde6168d7917b3e25bc82fc
Leal <leealalves@hotmail.com>
2023-11-09 21:58:17 -0300

    adicionada query para usuarios sem dispositivos

--- controller/usuarios/listarUnico.php
+++ controller/usuarios/listarUnico.php
@@ -5,21 +5,29 @@ $conexao = novaConexao();
 
 $cpf = $_POST['cpf'];
 
-$querySelect = "SELECT u.cpf, u.nome, d.* 
+$firstSelect = "SELECT u.cpf, u.nome, d.* 
                   FROM usuario u
                   JOIN dispositivo d 
                   ON u.id_conta = d.id_conta
                   WHERE cpf = $cpf";
 
+$secondSelect = "SELECT * 
+                  FROM usuario
+                  WHERE cpf = $cpf";
 
 try {
-
-  $result = $conexao->query($querySelect);
+  $result = $conexao->query($firstSelect);
   if ($result->rowCount() > 0) {
     $result = $result->fetchAll(PDO::FETCH_ASSOC);
     echo json_encode($result);
   } else {
-    echo json_encode(array("error" => "CPF nao encontrado."));
+    $result = $conexao->query($secondSelect);
+    if ($result->rowCount() > 0) {
+      $result = $result->fetchAll(PDO::FETCH_ASSOC);
+      echo json_encode($result);
+    } else {
+      echo json_encode(array("error" => "CPF nao encontrado."));
+    }
   }
 
 } catch (PDOException $e) {
