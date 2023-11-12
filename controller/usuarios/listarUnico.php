8da8164035ac6e6c29824eb8335778f31b3e4042
Vinicius Leal Alves <leealalves@hotmail.com>
2023-11-12 19:00:33 -0300

    querys refatoradas

--- controller/usuarios/listarUnico.php
+++ controller/usuarios/listarUnico.php
@@ -5,31 +5,30 @@ $conexao = novaConexao();
 
 $cpf = $_POST['cpf'];
 
-$firstSelect = "SELECT * 
-                  FROM usuario
-                  WHERE cpf = $cpf";
-
 try {
-  $consulta = $conexao->query($firstSelect);
-  if ($consulta->rowCount() > 0) {
-    $result1 = $consulta->fetchAll(PDO::FETCH_ASSOC);
+  $firstSelect = $conexao->prepare("SELECT * FROM usuario WHERE cpf = :cpf");
+  $firstSelect->bindParam(':cpf', $cpf, PDO::PARAM_STR);
+  $firstSelect->execute();
 
-    $secondSelect = "SELECT d.* 
-                  FROM usuario u
-                  JOIN dispositivo d 
-                  ON u.id_conta = d.id_conta
-                  WHERE cpf = $cpf";
+  if ($firstSelect->rowCount() > 0) {
+    $result1 = $firstSelect->fetchAll(PDO::FETCH_ASSOC);
 
-    $consulta2 = $conexao->query($secondSelect);
-    if ($consulta2->rowCount() > 0) {
-      $result2 = $consulta2->fetchAll(PDO::FETCH_ASSOC);
-      echo json_encode($result1[0] + Array("dispositivos" => $result2));
+    $secondSelect = $conexao->prepare("SELECT d.* 
+                      FROM usuario u
+                      JOIN dispositivo d 
+                      ON u.id_conta = d.id_conta
+                      WHERE cpf = :cpf");
+    $secondSelect->bindParam(":cpf", $cpf, PDO::PARAM_STR);
+    $secondSelect->execute();
+    if ($secondSelect->rowCount() > 0) {
+      $result2 = $secondSelect->fetchAll(PDO::FETCH_ASSOC);
+      echo json_encode($result1[0] + array("dispositivos" => $result2));
     } else {
       echo json_encode($result1[0]);
     }
 
   } else {
-    echo json_encode(array("error" => "CPF nao encontrado."));    
+    echo json_encode(array("error" => "CPF nao encontrado."));
   }
 
 } catch (PDOException $e) {
