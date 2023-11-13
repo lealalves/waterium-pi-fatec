2062bd89d11138ddc8f4c420a1597ada25e3346b
Vinicius Leal Alves <leealalves@hotmail.com>
2023-11-12 23:22:37 -0300

    identacao

--- view/usuarios/dispositivos.php
+++ view/usuarios/dispositivos.php
@@ -62,7 +62,6 @@
                 <th data-value="nome" scope="col">Nome</th>
                 <th data-value="cpf" scope="col">CPF</th>
                 <th data-value="codigo_dispositivo" scope="col">ID Aparelho</th>
-                <th scope="col">Bairro</th>
                 <th scope="col"></th>
               </tr>
             </thead>
@@ -75,7 +74,21 @@
       </main>
     </div>
   </div>
+  <script async
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBQwG7kSVo3XFVSaAt3tfLSt8fAwuM0XvY&&libraries=places">
+    </script>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js"
+    integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js"
+    integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha"
+    crossorigin="anonymous"></script>
+  <script src="script.js"></script>
   <script>
+    let txtBairro = document.querySelector("#txtBairro")
     let userList
 
     function sortBy(event) {
@@ -109,7 +122,6 @@
     }
 
     const setUserList = (arr) => {
-      console.log(arr);
       let table = document.querySelector("#userList")
 
       while (table.firstChild) {
@@ -124,7 +136,6 @@
           <td>${item.nome}</td>
           <td>${item.cpf}</td>
           <td>${item.codigo_dispositivo != null ? item.codigo_dispositivo : "SEM DISPOSITIVO"}</td>
-          <td>BAIRRO</td>
           <td onClick="redirectPage(event)">
             <button data-value="${item.cpf}" type="button" class="btn btn-secondary">
               Detalhes
@@ -133,12 +144,6 @@
           `
         table.append(linha)
       })
-    }
-    
-    const deleteUser = async (e) => {
-      const id = e.target.dataset.value
-      const res = await fetch(`http://localhost:80/waterium-pi-fatec/controller/usuarios/deletar.php?id=${id}`)
-      console.log(await res.json());
     }
 
     const redirectPage = (e) => {
@@ -151,16 +156,6 @@
       getUserList()
     };
   </script>
-
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js"
-    integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE"
-    crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js"
-    integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha"
-    crossorigin="anonymous"></script>
-  <script src="script.js"></script>
 </body>
 
 </html>
