9e2ffb27f8cf0d7f05ecbc14b88ba482f357c8ce
Vinicius Leal Alves <leealalves@hotmail.com>
2023-11-13 13:37:24 -0300

    controlador listarelatorios refatorado

--- controller/dispositivos/listarRelatorios.php
+++ controller/dispositivos/listarRelatorios.php
@@ -3,17 +3,25 @@ require_once("../conexao.php");
 
 $conexao = novaConexao();
 
-$querySelect = "SELECT u.cpf, u.nome, d.*, r.* 
-                FROM usuario u
-                JOIN dispositivo d 
-                ON u.id_conta = d.id_conta
-                join relatorio r 
-                on r.codigo_dispositivo = d.codigo_dispositivo";
-
 try {
+  $consulta;
+  if (isset($_GET['dcod'])) {
+    $id_dispositivo = $_GET['dcod'];
+    $consulta = $conexao->prepare("SELECT u.cpf, u.nome, d.*, r.* 
+                                   FROM usuario u
+                                   JOIN dispositivo d ON u.id_conta = d.id_conta
+                                   JOIN relatorio r ON r.codigo_dispositivo = d.codigo_dispositivo
+                                   WHERE d.codigo_dispositivo = :id_dispositivo");
+    $consulta->bindParam(':id_dispositivo', $id_dispositivo, PDO::PARAM_INT);
+  } else {
+    $consulta = $conexao->prepare("SELECT u.cpf, u.nome, d.*, r.* 
+                                   FROM usuario u
+                                   JOIN dispositivo d ON u.id_conta = d.id_conta
+                                   JOIN relatorio r ON r.codigo_dispositivo = d.codigo_dispositivo");
+  }
 
-  $result = $conexao->query($querySelect)->fetchAll(PDO::FETCH_ASSOC);
-
+  $consulta->execute();
+  $result = $consulta->fetchAll(PDO::FETCH_ASSOC);
   echo json_encode($result);
 
 } catch (PDOException $e) {
