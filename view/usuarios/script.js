fcec7464516dc0f2102aabc4a875c6f4bd9a2077
Vinicius Leal Alves <leealalves@hotmail.com>
2023-11-13 14:30:07 -0300

    funcao para dados aleatorios

--- view/usuarios/script.js
+++ view/usuarios/script.js
@@ -1,11 +1,36 @@
 feather.replace({ 'aria-hidden': 'true' })
 
-var colors = ['#007bff','#28a745','#333333','#c3e6cb','#dc3545','#6c757d'];
+let colors = ['#007bff', '#28a745', '#333333', '#c3e6cb', '#dc3545', '#6c757d'];
 
-var chBarPh = document.getElementById("chBarPh");
-var chBarFluoreto = document.getElementById("chBarFluoreto");
-var chBarTurbidez = document.getElementById("chBarTurbidez");
-var chBarCloro = document.getElementById("chBarCloro");
+let chBarPh = document.getElementById("chBarPh");
+let chBarFluoreto = document.getElementById("chBarFluoreto");
+let chBarTurbidez = document.getElementById("chBarTurbidez");
+let chBarCloro = document.getElementById("chBarCloro");
+
+const randomizeData = (originalData) => {
+  // Função para gerar um número aleatório com n casas decimais
+  function randomDecimal(n) {
+    return parseFloat((Math.random() * (10 ** n)).toFixed(n));
+  }
+
+  // Mapeia os dados originais para dados randomizados com o mesmo número de casas decimais
+  const randomizedData = originalData.map(value => {
+    // Verifica se o valor é um número e possui casas decimais
+    if (typeof value === 'number' && value % 1 !== 0) {
+      const decimalPlaces = value.toString().split('.')[1].length;
+      return randomDecimal(decimalPlaces);
+    } else {
+      return value; // Mantém valores não numéricos inalterados
+    }
+  });
+
+  return randomizedData;
+}
+
+const data1 = [7.50, 6.10, 5.70];
+const data2 = [0.03, 0.07, 0.05];
+const data3 = [1.20, 1, 1.50];
+const data4 = [0.80, 1, 0.50];
 
 const options = {
   legend: {
@@ -15,7 +40,7 @@ const options = {
     yAxes: [{
       ticks: {
         beginAtZero: true
-      }      
+      }
     }],
     xAxes: [{
       barPercentage: 0.2,
@@ -23,15 +48,14 @@ const options = {
     }]
   }
 }
-
-const generateCharts = () => {  
+const generateCharts = () => {
   new Chart(chBarPh, {
     type: 'bar',
     data: {
       labels: ["Setembro", "Outubro", "Novembro"],
       datasets: [{
         label: "ph",
-        data: [7.50, 6.10, 5.70],
+        data: randomizeData(data1),
         backgroundColor: colors[0],
       }]
     },
@@ -50,7 +74,7 @@ const generateCharts = () => {
       labels: ["Setembro", "Outubro", "Novembro"],
       datasets: [{
         label: "ph",
-        data: [0.03, 0.07, 0.05],
+        data: randomizeData(data2),
         backgroundColor: colors[1],
       }]
     },
@@ -69,7 +93,7 @@ const generateCharts = () => {
       labels: ["Setembro", "Outubro", "Novembro"],
       datasets: [{
         label: "ph",
-        data: [1.20, 1, 1.50],
+        data: randomizeData(data3),
         backgroundColor: colors[2],
       }]
     },
@@ -88,7 +112,7 @@ const generateCharts = () => {
       labels: ["Setembro", "Outubro", "Novembro"],
       datasets: [{
         label: "ph",
-        data: [0.80, 1, 0.50],
+        data: randomizeData(data4),
         backgroundColor: colors[5],
       }]
     },
