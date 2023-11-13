bed2e80b3cb84b1d077a2cc7767d4aa8d6177863
Vinicius Leal Alves <leealalves@hotmail.com>
2023-11-12 23:57:17 -0300

    controlador update nome usuario

---
+++ controller/usuarios/atualizar.php
@@ -0,0 +1,28 @@
+<?php
+require_once("../conexao.php");
+
+$nome = $_POST["nome"];
+$cpf = $_POST["cpf"];
+
+$conexao = novaConexao();
+
+try {
+  $updateQuery = $conexao->prepare("UPDATE usuario SET nome = :nome WHERE cpf = :cpf");
+  $updateQuery->bindParam(':cpf', $cpf, PDO::PARAM_STR);
+  $updateQuery->bindParam(':nome', $nome, PDO::PARAM_STR);
+  $updateQuery->execute();
+
+  if ($updateQuery->rowCount() > 0) {
+    echo json_encode("Nome atualizado com sucesso.");
+  } else {
+    echo json_encode("Erro ao atualizar nome de usuário.");    
+  }
+
+} catch (PDOException $e) {
+  echo json_encode($e->getMessage());
+  exit;
+}
+
+
+
+?>
